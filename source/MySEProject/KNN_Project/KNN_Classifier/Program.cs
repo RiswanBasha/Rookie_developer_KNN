5179145805d847ecfbbcb1431ad4160879095ca0
unknown <riswanbasha26@gmail.com>
2023-01-22 21:14:47 +0100

    22.01.2023/Riswan_3

---
+++ source/MySEProject/KNN_Project/KNN_Classifier/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.PortableExecutable;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to KNN Classifier Project by Rookie Developers");
+
+        // This project deals with the investigation and implementation of KNN Classfiers.
+        static double Distance(double[] unknown, double[] data)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < unknown.Length; ++i)
+                sum += (unknown[i] - data[i]) * (unknown[i] - data[i]);
+            return Math.Sqrt(sum);
+        }
+
+    }
+}
